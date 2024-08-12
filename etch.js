d959bc9f2744e0d4a2cd990029b0834a7b0ababd
Zaqzulkan <zulkanzaqzuq@gmail.com>
2024-08-12 17:47:35 +0700

    Creating the files needed

---
+++ etch.js
@@ -0,0 +1,10 @@
+const body = document.body;
+
+let div1 = document.createElement("div");
+div1.style.display = "flex";
+div1.style.margin = "50px";
+div1.style.width = "300px";
+div1.style.backgroundColor = "black";
+body.append(div1);
+
+console.log("Div appended");
